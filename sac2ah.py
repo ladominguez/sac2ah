e59238e013a9920a70ddd866e36962eb604ef014
Luis Antonio Dominguez <ladominguez@ucla.edu>
2022-07-04 22:38:23 -0500

    Response option added.

--- sac2ah.py
+++ sac2ah.py
@@ -2,29 +2,38 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-from tkinter import W
+import os
 from obspy.core import read
 from obspy.core.inventory import read_inventory
 
 instrument_response='/Users/antonio/Dropbox/espectrosGolfo/InstrumentResponse'
-type = {6:'Disp (nm)', 7:'Vel (nm/s)', 8:'Acc (nm/s/s)'}
+type = {5:'Unknown',6:'Disp (nm)', 7:'Vel (nm/s)', 8:'Acc (nm/s/s)'}
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-i', '--input', type=str, help='input file (sac).')
     parser.add_argument('-o', '--output', type=str, help='output file (sac).')
+    parser.add_argument('-r', '--response', type=str, default=instrument_response, help='output file (sac).')
     args = parser.parse_args()
     input = args.input
     output = args.output
+    resp_file = args.response	
     sac = read(input)
     stnm =sac[0].stats.sac.kstnm.strip() 
-    inv = read_inventory(instrument_response + '/*' + stnm + '*', format='RESP')
+    if os.path.isfile(resp_file):
+        inv = read_inventory(resp_file, format='RESP')
+    elif os.path.isdir(resp_file):
+        inv = read_inventory(resp_file + '/*' + stnm + '*', format='RESP')
     response=inv.get_response(sac[0].get_id(),sac[0].stats.starttime)
     paz=response.get_paz()
     normalization_factor=paz.normalization_factor
     gain=response.instrument_sensitivity.value
     poles=paz.poles
     zeros=paz.zeros
+    try:
+        idep=sac[0].stats.sac.idep
+    except:
+        idep=5
     with open(output, 'w') as fout:
         fout.write('station information\n')
         fout.write('code:\t' + sac[0].stats.sac.kstnm.strip() + '\n')
@@ -37,7 +46,8 @@ if __name__ == '__main__':
         fout.write('normaliztion:\t'+'%.2f'%normalization_factor+'\n')
         fout.write('calibration information\n')
         fout.write('pole.re		pole.im		zero.re		zero.im \n')
-        for k in range(0, 30):
+        fout.write('%8.6e' % len(poles)+'\t'+'0.000000e+00'+'\t'+'%8.6e' % len(zeros)+'\t'+'0.000000e+00\n')
+        for k in range(0, 29):
             try:
                 pole=poles[k]
                 pole_real=pole.real
@@ -69,7 +79,7 @@ if __name__ == '__main__':
         fout.write('start_time: '+str(sac[0].stats.starttime.year)+'\t'+str(sac[0].stats.starttime.month)+'\t'+str(sac[0].stats.starttime.day)+'\t'+str(
             sac[0].stats.starttime.hour)+'\t'+str(sac[0].stats.starttime.minute)+'\t'+'%8.6f' % sac[0].stats.starttime.second+'\n')
         fout.write('abscissa_min:\t0.000000e+00\n')
-        fout.write('comment:\tComp azm=%4.1f,inc=%4.1f; ' %(sac[0].stats.sac.cmpaz,sac[0].stats.sac.cmpinc) + type[sac[0].stats.sac.idep] +'\n')
+        fout.write('comment:\tComp azm=%4.1f,inc=%4.1f; ' %(sac[0].stats.sac.cmpaz,sac[0].stats.sac.cmpinc) + type[idep] +'\n')
         fout.write('log:\tnull\n')
         fout.write('extras:\n')
         for k in range(21):
